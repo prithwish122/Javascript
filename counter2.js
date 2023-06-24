58a0fe3f93e61ed734b6a85bb0fa7a7f4a9c1bee
Prithwish Chatterjee <116712239+prithwish122@users.noreply.github.com>
2023-06-24 17:35:00 +0530

    Create counter2.js

---
+++ counter2.js
@@ -0,0 +1,22 @@
+var createCounter = function(init) {
+    let c=init;
+    function increment(){
+        c++;
+        return c;
+    }
+    function decrement()
+    {
+        c--;
+        return c;
+
+    }
+    function reset(){
+        c=init;
+        return c;
+    }
+    return {
+        increment,
+        decrement,
+        reset
+    };
+};
